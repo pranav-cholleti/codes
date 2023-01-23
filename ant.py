7f3ef253d951134ffebd033450b0c637f65ca9e6
pranav-cholleti <119649614+pranav-cholleti@users.noreply.github.com>
2023-01-23 21:53:07 +0530

    Update ant.py

--- ant.py
+++ ant.py
@@ -2,12 +2,11 @@ import pandas as pd
 import numpy as np
 from pyodide.http import open_url
 url = ("https://raw.githubusercontent.com/pranav-cholleti/codes/main/hospitals.csv")
-print("Hello World")
 data_set=pd.read_csv(open_url(url))
 print(data_set)
 print()
 print()
-print(data_set.iloc[0,:])
+#print(data_set.iloc[0,:])
 location = input("Enter your location: ")
 
 min_dist_index=-1
